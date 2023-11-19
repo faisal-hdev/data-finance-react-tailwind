f635fb53584cd631d0d2e7bece5de088700e17a2
faisal-hdev <itsfaisalchowdhury@gmail.com>
2023-11-19 16:43:43 +0600

    Funtionalnavbar feature are added

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="text-white">
+      <h2>Hero</h2>
+    </div>
+  );
+};
+
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { useState } from "react";
+import { RiMenu2Fill } from "react-icons/ri";
+import { IoCloseSharp } from "react-icons/io5";
 
 const Navbar = () => {
-    return (
-        <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-            <h1 className='w-full text-3xl font-bold text-[#00df9a]'>React.</h1>
-            <ul className='flex'>
-                <li className='p-4'>Home</li>
-                <li className='p-4'>Company</li>
-                <li className='p-4'>Resources</li>
-                <li className='p-4'>About</li>
-                <li className='p-4'>Contact</li>
-            </ul>
-        </div>
-    );
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+      <h1 className="w-full text-3xl font-bold text-[#00df9a]">
+        Data-finance.
+      </h1>
+      <ul className="md:flex hidden">
+        <li className="p-4">Home</li>
+        <li className="p-4">Company</li>
+        <li className="p-4">Resources</li>
+        <li className="p-4">About</li>
+        <li className="p-4">Contact</li>
+      </ul>
+
+      <div onClick={handleNav} className="block md:hidden">
+        {!nav ? <IoCloseSharp size={24} /> : <RiMenu2Fill size={24} />}
+      </div>
+
+      <div
+        className={
+          !nav
+            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
+            : "fixed left-[100%]"
+        }
+      >
+        <h1 className="w-full text-3xl font-bold m-4 text-[#00df9a]">
+          Data-finance.
+        </h1>
+        <ul className="p-4 uppercase">
+          <li className="p-4 border-b border-gray-600">Home</li>
+          <li className="p-4 border-b border-gray-600">Company</li>
+          <li className="p-4 border-b border-gray-600">Resources</li>
+          <li className="p-4 border-b border-gray-600">About</li>
+          <li className="p-4">Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
